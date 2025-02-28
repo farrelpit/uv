0ad1edbc3ffb719b6f877cee5973befc3f5e0f55
farrelpit <farrelputra141@gmail.com>
2025-02-28 10:58:14 +0700

    Tes1

---
+++ tes1/main.py
@@ -0,0 +1,11 @@
+# function
+
+
+
+
+def main():
+    print("Hello from tes1!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tes1/rumus_matematika.py
@@ -0,0 +1,9 @@
+class RumusMatemika:
+    def luas_persegi(sisi):
+        return sisi * sisi
+
+    def luas_lingkaran(jari_jari):
+        return 3.14 * jari_jari * jari_jari
+
+    def luas_segitiga(alas, tinggi):
+        return 0.5 * alas * tinggi
