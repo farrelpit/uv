663d9ee357ee7d3c7cf6875c4ec3ce5b6af0c28c
farrelpit <farrelputra141@gmail.com>
2025-03-06 18:39:58 +0700

    Delete tes1 directory

--- tes1/rumus_matematika.py
+++
@@ -1,9 +0,0 @@
-class RumusMatemika:
-    def luas_persegi(sisi):
-        return sisi * sisi
-
-    def luas_lingkaran(jari_jari):
-        return 3.14 * jari_jari * jari_jari
-
-    def luas_segitiga(alas, tinggi):
-        return 0.5 * alas * tinggi
